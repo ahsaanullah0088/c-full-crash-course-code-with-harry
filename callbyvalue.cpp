838bf2966afd07545d34492264701b9f556181da
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-05-03 19:21:27 +0500

    call by refrence and call  by value inc ++

--- callbyvalue.cpp
+++ callbyvalue.cpp
@@ -1,4 +1,12 @@
-
+#include<iostream>
+using namespace std;
+    // int sum ( int a , int b){
+    //     int c = a+b;
+    //     return c;
+    // }
+ void swapPointer ( int* a , int* b){
+        int temp = *a;
+        *a = *b;
         *b = temp;
     }
 int main(){
